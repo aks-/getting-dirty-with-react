eba95a58af16f3f5a980bd7619a9fac29592157e
aks- <coderatlabs@gmail.com>
2015-12-08 12:02:11 +0530

    create first stateless component

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-class App extends Component {
-  render() {
-    return <div>Howdy</div>;
-  }
-}
+/* class App extends Component { */
+  // render() {
+    // return <div>Howdy</div>;
+  // }
+/* } */
+
+const App = () => <div>Howdy</div>;
 
 export default App;
