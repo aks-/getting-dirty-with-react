a982f16776bedd89ccbd3184ec91701e7ac5ac9b
aks- <coderatlabs@gmail.com>
2015-12-08 12:11:35 +0530

    Default proptypes and types of props

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-/* class App extends Component { */
-  // render() {
-    // return <div>Howdy</div>;
-  // }
-/* } */
+class App extends Component { 
+  render() {
+    let num = this.props.num;
+    let txt = this.props.txt;
+    let result = txt + " " + num;
+    return <div>{result}</div>;
+  }
+} 
 
-const App = () => <div>Howdy</div>;
+App.propTypes = {
+  txt: React.PropTypes.string,
+  num: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: 'Hey your number is'
+};
 
 export default App;
